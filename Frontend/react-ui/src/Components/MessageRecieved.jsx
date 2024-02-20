9d0fd15d144c6a9b23a5c0e7e207d44492197cdd
Pravinkumar S <spravinkumar9952@gmail.com>
2024-02-20 23:51:32 +0530

    message sent ui fix

--- Frontend/react-ui/src/Components/MessageRecieved.jsx
+++ Frontend/react-ui/src/Components/MessageRecieved.jsx
@@ -1,15 +1,10 @@
 
+export default function MessageReceived({message, time, type}){
 
-
-export default function MessageSent({message, time, type}){
-    console.log("Type ", type);
     return(
-        <div className="w-screen">
-            <div className="w-fit ml-auto mr-4 px-2 mt-2 rounded-md rounded-br-none bg-green-100 flex flex-row">
-                {type === "Text" && <h1 className="p-1 m-1 ">{message} <sub className="timestamp-text ml-2">{time}</sub></h1>}
-                {type === "Image" && <img className="p-1 m-1 w-52 h-52 object-contain" src = {message} />}
-            </div>
+        <div className="w-fit px-2 m-4 rounded-md bg-blue-100 rounded-bl-none">
+            {type === "Text" && <h1 className="p-1 m-1 max-w-xs">{message} <sub className="timestamp-text ml-2">{time}</sub></h1>}
+            {type === "Image" && <img className="p-1 m-1 w-52 h-52 object-contain" src = {message} />}
         </div>
-
     )
 }
