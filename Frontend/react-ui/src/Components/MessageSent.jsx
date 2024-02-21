a4fa741f7a96190b024b92214157f1cb47b06b66
Pravinkumar S <spravinkumar9952@gmail.com>
2024-02-21 23:08:43 +0530

    message sent ui fix 2

--- Frontend/react-ui/src/Components/MessageSent.jsx
+++ Frontend/react-ui/src/Components/MessageSent.jsx
@@ -1,11 +1,15 @@
 
 
 
-export default function MessageSent({message}){
-
+export default function MessageSent({message, time, type}){
+    console.log("Type ", type);
     return(
-        <div className="mt-6 d-flex justify-content-end">
-            <h1 className="p-2 m-4 ml-20 mt-6 rounded bg-green-100">{message}</h1>
+        <div className="w-screen">
+            <div className="w-fit ml-auto mr-4 px-2 mt-2 rounded-md rounded-br-none bg-green-100 flex flex-row">
+                {type === "Text" && <h1 className="p-1 m-1 ">{message} <sub className="timestamp-text ml-2">{time}</sub></h1>}
+                {type === "Image" && <img className="p-1 m-1 w-52 h-52 object-contain" src = {message} />}
+            </div>
         </div>
+
     )
 }
